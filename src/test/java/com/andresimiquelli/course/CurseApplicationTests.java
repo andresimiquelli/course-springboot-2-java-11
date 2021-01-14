a266130f27d5f21799457603d30206cab7cfa102
André Simiquelli <andresimiquelli@hotmail.com>
2021-01-14 20:59:20 -0300

    Project created

---
+++ src/test/java/com/andresimiquelli/course/CurseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andresimiquelli.course;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
